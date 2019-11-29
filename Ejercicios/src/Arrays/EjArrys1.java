ee872556cfcce94acf174332901d66a9ef6e8eb4
lucia <lucia@192.168.1.201>
2019-11-29 10:56:35 +0100

    Arrays

---
+++ Ejercicios/src/Arrays/EjArrys1.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class EjArrys1 {
+
+	public static void main(String[] args) {
+		int[] miPrimerArray=new int[3];
+		
+		miPrimerArray[0]=31;
+		miPrimerArray[1]=28;
+		miPrimerArray[2]=31;
+		System.out.println(Arrays.toString(miPrimerArray));
+		System.out.println(miPrimerArray[0]);
+		String[] miSegundoArray=new String [2];
+		
+		miSegundoArray[0]="Sevilla";
+		miSegundoArray[1]="Madrid";
+		System.out.println(Arrays.toString(miSegundoArray));
+
+		
+		
+		
+	}
+
+	
+}
